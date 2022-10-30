15f39251ae82a25d0a194883df47212985f88029
Om <82279638+om251@users.noreply.github.com>
2022-10-30 16:02:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(300, 300);
+    canvas.position(100, 250)
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save("T[owudiygef].png");
+}
